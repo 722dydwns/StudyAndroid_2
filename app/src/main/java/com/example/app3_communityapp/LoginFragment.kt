300de673cf7006c032267f10894fa6d8f455474d
YongJOoOo <we072200@naver.com>
2022-08-04 12:16:22 +0900

    TIL 220804

--- app/src/main/java/com/example/app3_communityapp/LoginFragment.kt
+++ app/src/main/java/com/example/app3_communityapp/LoginFragment.kt
@@ -1,11 +1,14 @@
 package com.example.app3_communityapp
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import com.example.app3_communityapp.databinding.FragmentLoginBinding
 
 
@@ -35,9 +38,50 @@ class LoginFragment : Fragment() { //'로그인' 프래그먼트
         }
         // '로그인' 버튼 클릭 시, 이벤트 처리 :
         loginFragmentBinding.loginLoginbtn.setOnClickListener {
-            val boardMainIntent = Intent(requireContext(), BoardMainActivity::class.java)
-            startActivity(boardMainIntent)
-            activity?.finish()
+
+            //사용자 입력 데이터 받기
+            val loginId = loginFragmentBinding.loginId.text.toString()
+            val loginPw = loginFragmentBinding.loginPw.text.toString()
+            val chk = loginFragmentBinding.loginAutologin.isChecked //자동 로그인 체크 여부
+
+            var loginAutoLogin = 0 //자동 로그인 여부 분기 처리
+            if(chk == true) {
+                loginAutoLogin = 1
+            }else {
+                loginAutoLogin = 0
+            }
+            //유효성 검사 - 입력 여부에 따른 분기
+            //로그인 ID 값
+            if(loginId == null || loginId.length == 0 ){
+                val dialogBuilder = AlertDialog.Builder(requireContext())
+                dialogBuilder.setTitle("아이디 입력 오류")
+                dialogBuilder.setMessage("아이디를 입력해주세요 ")
+                dialogBuilder.setPositiveButton("확인"){ dialogInterface: DialogInterface, i: Int ->
+                    loginFragmentBinding.loginId.requestFocus() //포커스 주기
+                }
+                dialogBuilder.show()
+                return@setOnClickListener
+            }
+            //로그인 PW 값
+            if(loginPw == null || loginPw.length == 0) {
+                val dialogBuilder = AlertDialog.Builder(requireContext())
+                dialogBuilder.setTitle("비밀번호 입력 오류")
+                dialogBuilder.setMessage("비밀번호를 입력해주세요 ")
+                dialogBuilder.setPositiveButton("확인"){ dialogInterface: DialogInterface, i: Int ->
+                    loginFragmentBinding.loginPw.requestFocus()
+                }
+                dialogBuilder.show()
+                return@setOnClickListener
+            }
+            //데이터 잘 받아졌는지 Log 찍기
+            Log.d("test", loginId)
+            Log.d("test", loginPw)
+            Log.d("test", "$loginAutoLogin")
+
+            //화면 전환 -> 보드 메인
+           // val boardMainIntent = Intent(requireContext(), BoardMainActivity::class.java)
+           // startActivity(boardMainIntent)
+           // activity?.finish()
         }
 
         return loginFragmentBinding.root
