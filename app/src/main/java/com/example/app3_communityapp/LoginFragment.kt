c1dc0ae0311d7c98533d100fcb5a242e6f4a8b3f
YongJOoOo <we072200@naver.com>
2022-08-05 14:40:46 +0900

    TIL 220805

--- app/src/main/java/com/example/app3_communityapp/LoginFragment.kt
+++ app/src/main/java/com/example/app3_communityapp/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.example.app3_communityapp
 
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
@@ -10,6 +11,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import com.example.app3_communityapp.databinding.FragmentLoginBinding
+import okhttp3.FormBody
+import okhttp3.OkHttp
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import kotlin.concurrent.thread
 
 
 class LoginFragment : Fragment() { //'로그인' 프래그먼트
@@ -78,14 +84,73 @@ class LoginFragment : Fragment() { //'로그인' 프래그먼트
             Log.d("test", loginPw)
             Log.d("test", "$loginAutoLogin")
 
-            //화면 전환 -> 보드 메인
-           // val boardMainIntent = Intent(requireContext(), BoardMainActivity::class.java)
-           // startActivity(boardMainIntent)
-           // activity?.finish()
-        }
+            //-> 서버와 통신 처리
+            thread {
+                val client = OkHttpClient()
+                val site = "http://172.30.1.27:8080/App3_CommunityServer/login_user.jsp"
 
+                //서버로 전달할 데이터 몸통 세팅 처리 - id/pw 값을 전달하고
+                val builder1 = FormBody.Builder()
+                builder1.add("user_id", loginId)
+                builder1.add("user_pw", loginPw)
+                builder1.add("user_autologin", "$loginAutoLogin")
+                val formBody = builder1.build()
+
+                val request = Request.Builder().url(site).post(formBody).build()
+                val response = client.newCall(request).execute() //요청 수행
+
+                //통신 성공 여부에 따라 분기
+                if(response.isSuccessful == true) { //통신 정상 연결
+                    //응답 결과를 추출
+                    val result_text = response.body?.string()!!.trim()
+
+                    if(result_text == "FAIL" ) { //로그인 실패 시 처리
+                        activity?.runOnUiThread{
+                            val dialogBuilder = AlertDialog.Builder(requireContext())
+                            dialogBuilder.setTitle("로그인 실패")
+                            dialogBuilder.setMessage("아이디나 비번 잘못되었습니다.")
+                            dialogBuilder.setPositiveButton("확인"){ dialogInterface: DialogInterface, i: Int ->
+                                //입력한 칸 비워두고 - 자동로그인 체크 해제 - 자동 아이디 포커싱
+                                loginFragmentBinding.loginId.setText("")
+                                loginFragmentBinding.loginPw.setText("")
+                                loginFragmentBinding.loginAutologin.isChecked = false
+                                loginFragmentBinding.loginId.requestFocus()
+                            }
+                            dialogBuilder.show()
+                        }
+                    } else{ //로그인 성공 시 처리
+                        activity?.runOnUiThread {
+                            val dialogBuilder = AlertDialog.Builder(requireContext())
+                            dialogBuilder.setTitle("로그인 성공")
+                            dialogBuilder.setMessage("로그인에 성공하였습니다.")
+                            dialogBuilder.setPositiveButton("확인"){ dialogInterface: DialogInterface, i: Int ->
+
+                                // 사용자 정보를 'preferences' 에 저장한다.
+                                val pref = activity?.getSharedPreferences("login_data", Context.MODE_PRIVATE)
+                                val editor = pref?.edit()
+                                //서버로부터 전달받는 데이터는 모두 String 형태이므로 받을 때 정수형 Integer로 형변환시켜서 받음
+                                //putString(키, 값) , putInt(키, 값) 형태
+                                editor?.putInt("login_user_idx", Integer.parseInt(result_text)) //해당 사용자 idx
+                                editor?.putInt("login_auto_login", loginAutoLogin) //자동로그인 체크 여부
+                                editor?.commit()
+
+                                //로그인 성공 후 -> 화면 전환처리
+                                val boardMainIntent = Intent(requireContext(), BoardMainActivity::class.java)
+                                startActivity(boardMainIntent)
+                                activity?.finish()
+                            }
+                            dialogBuilder.show() //알림 띄우기
+                        }
+                    }
+                }else{ //통신 비정상인 경우
+                    val dialogBuilder = AlertDialog.Builder(requireContext())
+                    dialogBuilder.setTitle("로그인 오류")
+                    dialogBuilder.setMessage("로그인 오류 발생")
+                    dialogBuilder.setPositiveButton("확인", null)
+                    dialogBuilder.show()
+                }
+            }
+        }
         return loginFragmentBinding.root
     }
-
-
 }
--- app/src/main/java/com/example/app3_communityapp/MainActivity.kt
+++ app/src/main/java/com/example/app3_communityapp/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.app3_communityapp
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.app3_communityapp.databinding.ActivityMainBinding
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() { //'메인' 액티비티
     //바인딩 설정
@@ -17,14 +23,45 @@ class MainActivity : AppCompatActivity() { //'메인' 액티비티
     var userPw = ""
     var userNickName = ""
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainActivityBinding.root)
-        //프래그먼트 컨트롤 메소드
-        fragmentController("login", false, false)
+
+        //Preference에 저장된 현재 User 정보를 가져옴
+        // + 해당 User의 자동 로그인 희망 여부에 따른 화면 전환 분기문
+        val pref = getSharedPreferences("login_data", Context.MODE_PRIVATE)
+        val login_user_idx = pref.getInt("login_user_idx", -1)
+        val login_auto_login = pref.getInt("login_auto_login", -1) //-1은 deValue
+
+        if(login_auto_login == 1) { //자동로그인 희망 O
+            thread {
+                //서버로 보낼 데이터 세팅
+                val client = OkHttpClient()
+                val builder1 = FormBody.Builder()
+                builder1.add("login_user_idx", "$login_user_idx")
+                val formBody = builder1.build()
+                //서버 통신을 위한 세팅
+                val site = "http://172.30.1.27:8080/App3_CommunityServer/check_auto_login.jsp"
+                val request = Request.Builder().url(site).post(formBody).build()
+                val response = client.newCall(request).execute()
+
+                if(response.isSuccessful == true) { //통신 성공 시
+                    val result_text = response.body?.string()!!.trim()
+                    val chk = Integer.parseInt(result_text)
+                    if(chk == 1) {
+                        val boardMainIntent = Intent(this, BoardMainActivity::class.java)
+                        startActivity(boardMainIntent)
+                        finish()
+                    }else {
+                        fragmentController("login", false, false)
+                    }
+                }
+            }
+        }else{
+            fragmentController("login", false, false)
+        }
     }
 
     //이 메인 액티비티가 관리할 프래그먼트 화면들을 컨트롤할 메소드
@@ -58,7 +95,6 @@ class MainActivity : AppCompatActivity() { //'메인' 액티비티
         }
 
         trans.commit() // 프래그먼트 동적 제어 - 화면에 위의 상황을 적용하는 메소드
-
     }
 
 }
